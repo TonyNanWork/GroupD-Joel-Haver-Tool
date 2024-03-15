bc19de29a8902cf534b39b7cc1c6c1f2a451c8a7
EggMcLegg <otto2002@live.ca>
2024-03-14 22:03:39 -0700

    commented out some prints, changed to .py filetype.

--- img2vid.py
+++ img2vid.py
@@ -1,9 +1,10 @@
 import cv2
 import os
 
+# Converts images in folder to video
 def images_to_video(image_folder, video_name, fps=30):
     images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
-    images.sort(key=lambda x: int(x.split('.')[0]))  # Sort images numerically
+    images.sort(key=lambda x: int(x.split('.')[0]))
     if not images:
         print("No images found in the folder: ", image_folder)
         return
@@ -19,8 +20,6 @@ def images_to_video(image_folder, video_name, fps=30):
     cv2.destroyAllWindows()
     video.release()
 
-
-if __name__ == "__main__":
-    folder_path = "output"
-    output_video_name = "output.avi"
-    images_to_video(folder_path, output_video_name)
+folder_path = "output"
+output_video_name = "output.avi"
+images_to_video(folder_path, output_video_name)
