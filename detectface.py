3d2acb98b631e911537905bb8b1a31a1c3c360a7
Tony Nan <70991973+EssentiallyWrong@users.noreply.github.com>
2024-04-03 03:44:50 -0700

    face detection

---
+++ detectface.py
@@ -0,0 +1,54 @@
+import cv2
+import dlib
+import numpy as np
+import os
+
+
+def process_frame(image_path):
+    # import required libraries
+
+    # read input image
+    img = cv2.imread(image_path)
+
+    # convert to grayscale of each frames
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # read the haarcascade to detect the faces in an image
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    # read the haarcascade to detect the eyes in an image
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+
+    # detects faces in the input image
+    faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+    print('Number of detected faces:', len(faces))
+    roi_gray = None
+    roi_color = None
+    # loop over the detected faces
+    for (x,y,w,h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+    
+    # detects eyes of within the detected face area (roi)
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    
+    # draw a rectangle around eyes
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,255),2)
+
+    # display the image with detected eyes
+    cv2.imshow('Eyes Detection',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+        
+
+
+
+
+# Process all PNG frames in a directory
+frames_directory = 'video_data1'
+for frame in os.listdir(frames_directory):
+    if frame.endswith('.jpg'):
+        process_frame(os.path.join(frames_directory, frame))
+
+cv2.destroyAllWindows()
--- propogate.py
+++ propogate.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Compute dense optical flow
 def compute_optical_flow(prev_frame, next_frame):
-    return cv2.calcOpticalFlowFarneback(prev_frame, next_frame, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    return cv2.calcOpticalFlowFarneback(prev_frame, next_frame, None, pyr_scale = 0.5, levels = 3, winsize = 15, iterations = 5, poly_n = 5, poly_sigma = 1.2, flags = 0)
 
 # warp drawn frame using flow
 def warp_frame(flow, drawn_frame, interpolation=cv2.INTER_LINEAR):
@@ -50,7 +50,7 @@ def propagate(video_frame_folder, drawn_frame_folder, output_frame_folder):
     # drawn_frames_mapping = {i: cv2.imread(os.path.join(drawn_frame_folder, frame)) for i, frame in enumerate(drawn_frames)}
     
     # Iterate over each video frame and propagate the drawn style
-    for i in range(len(video_frames) - 1):
+    for i in range(int(len(video_frames)/10)):
         video_frame = cv2.imread(os.path.join(video_frame_folder, video_frames[i]))
         next_video_frame = cv2.imread(os.path.join(video_frame_folder, video_frames[i+1]))
 
@@ -62,15 +62,15 @@ def propagate(video_frame_folder, drawn_frame_folder, output_frame_folder):
         #flow = compute_optical_flow(video_frame.mean(-1), next_video_frame.mean(-1))
         flow = compute_optical_flow(video_frame.mean(-1), drawn_frames[closest_drawn].mean(-1))
         
-        # flow_visualization = visualize_optical_flow(flow)
-        # flow_output_path = os.path.join("flow", f"flow_{os.path.splitext(video_frames[i])[0]}.png")
-        # cv2.imwrite(flow_output_path, flow_visualization)
+        flow_visualization = visualize_optical_flow(flow)
+        flow_output_path = os.path.join("flow", f"flow_{os.path.splitext(video_frames[i])[0]}.png")
+        cv2.imwrite(flow_output_path, flow_visualization)
         
         # Warp the frame
         warped = warp_frame(flow, drawn_frames[closest_drawn])
         
         # Save the frame
-        output_frame = os.path.join(output_frame_folder, f'{os.path.splitext(video_frames[i])[0]}.png')
+        output_frame = os.path.join(output_frame_folder, f'{os.path.splitext(video_frames[i])[0]}.jpg')
         cv2.imwrite(output_frame, warped)
 
 video_folder = 'video_data'
